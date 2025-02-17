129c866348a58adbd8d27e014e1d6e2d4c20d0a9
r333vil <karolbrach3@gmail.com>
2025-02-17 18:28:39 +0100

    Added 5 exercise

--- zadanie_45.js
+++ zadanie_45.js
@@ -1,2 +1,5 @@
 //Zadanie nr 45
 //Sprawdź, czy HTMLCollection jest obiektem array-like i wypisz jego długość.
+
+let htmlCollection = // ?? co tutaj  czy to ma byc pobranie czegos z html i sprawdzenie tego czy to jest array i length ?
+console.log([Array.isArray(htmlCollection), htmlCollection.length]);
--- zadanie_48.js
+++ zadanie_48.js
@@ -1,2 +1,9 @@
 //Zadanie nr 48
 //Używając slice.call(), przekonwertuj arguments na tablicę i zwróć jej elementy.
+
+function convert(){
+    const arg = Array.prototype.slice.call(arguments)
+    const argArray = Array.from(arg)
+    console.log(argArray);
+}
+convert("Zadanie nr 48")
