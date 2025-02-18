3964e875b30cc3ddf35612ab396d812f2692127f
r333vil <karolbrach3@gmail.com>
2025-02-18 16:35:33 +0100

    exercise 45

--- zadanie_45.js
+++ zadanie_45.js
@@ -1,5 +1,5 @@
 //Zadanie nr 45
 //Sprawdź, czy HTMLCollection jest obiektem array-like i wypisz jego długość.
 
-let htmlCollection = // ?? co tutaj  czy to ma byc pobranie czegos z html i sprawdzenie tego czy to jest array i length ?
+let htmlCollection = document.getElementsByClassName('cars');
 console.log([Array.isArray(htmlCollection), htmlCollection.length]);
